d3b8925235868048ad7bd12e17fcd248a69f06db
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-08 15:06:38 +0500

    Refactor Navbar component to include logo and improve layout for better navigation experience

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,27 @@
 import React from 'react'
+import logo from "@/assets/logo.png"
+import Link from 'next/link'
+import Image from 'next/image'
 
 function Navbar() {
-  return (
-    <nav className='bg-transparent'>
-        <div>logo</div>
-        <ul>
-            <li>Home</li>
-            <li>home</li>
-            <li>home</li>
-            <li>home</li>
-        </ul>
-    </nav>
-  )
+    return (
+        <nav className='absolute top-1 right-0 w-full h-20 z-3 flex flex-row justify-between items-center px-24'>
+            <Link
+                href="/"
+                aria-label="Homepage"
+                className="flex items-center gap-2 text-2xl font-mono font-semibold tracking-widest hover:opacity-90 transition-opacity text-white"
+            >
+                <Image width={10} height={10} src={logo} alt="Shoeify logo" className="w-10 h-10" />
+                <span>NextStep</span>
+            </Link>
+            <ul>
+                <li>Home</li>
+                <li>home</li>
+                <li>home</li>
+                <li>home</li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
