d85ec4bb99d385f35529fce6c4f4868df82a07e1
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-08 22:16:16 +0500

    Add initial GET route in getData API with error handling structure

---
+++ app/api/getData/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
+
+
+export async function GET(req: NextRequest, res: NextResponse) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
