0724bc8d21c7e0a78d8b2ddbcc9ac27fd3c434a4
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-08 15:01:16 +0500

    Add Navbar component to RootLayout for improved navigation

---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='bg-transparent'>
+        <div>logo</div>
+        <ul>
+            <li>Home</li>
+            <li>home</li>
+            <li>home</li>
+            <li>home</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
