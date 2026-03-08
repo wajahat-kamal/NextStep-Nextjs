51e71ee37ff6f46ae917371b1dab0ee99281cd4a
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-08 22:54:32 +0500

    Add "type": "module" to package.json and implement file reading in GET route for data retrieval

--- app/api/getData/route.ts
+++ app/api/getData/route.ts
@@ -1,11 +1,20 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import fs from "node:fs/promises";
+import path from "node:path";
 
-
-
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function GET() {
     try {
-        
-    } catch (error) {
-        
+        const filePath = path.join(process.cwd(), "ShoeData.json")
+        const data = await fs.readFile(filePath, "utf8")
+
+        return NextResponse.json({
+            success: true,
+            data: JSON.parse(data)
+        }, { status: 200 })
+    } catch (error: any) {
+        NextResponse.json({
+            success: false,
+            message: error.message
+        }, { status: 500 })
     }
 }
