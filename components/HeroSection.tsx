e6fe91b5ccf6ce9a92c4d3b39b8851323aade758
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-08 14:03:47 +0500

    Enhance HeroSection component with background image and updated layout for improved visual appeal.

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import bgImage from "@/assets/main-bg-image.jpg"
 
 function HeroSection() {
-  return (
-    <div>HeroSection</div>
-  )
+    return (
+        <header className='w-full h-screen bg-cover bg-fixed bg-no-repeat bg-center'
+            style={{ backgroundImage: `url(${bgImage})` }}
+        >
+            <div>
+                <h1>Hero Section</h1>
+            </div>
+        </header>
+    )
 }
 
 export default HeroSection
