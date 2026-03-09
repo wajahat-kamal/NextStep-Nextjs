45441aae06bdcbc9f27fa5db9471662001d9776b
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-09 16:41:18 +0500

    Add remote image patterns to Next.js configuration for improved asset handling

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      { hostname: "static.nike.com" },
+      { hostname: "assets.adidas.com" },
+      { hostname: "assets.reebok.com" },
+      { hostname: "images.vans.com" },
+      { hostname: "cdn.shopify.com" },
+    ],
+  },
 };
 
 export default nextConfig;
