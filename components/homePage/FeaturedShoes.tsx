4412e8a9b4584dff54bba09b3dd16722f55a02f8
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-10 11:51:06 +0500

    Integrate Header component into FeaturedShoes for improved layout and dynamic content presentation, enhancing user engagement and code reusability.

--- components/homePage/FeaturedShoes.tsx
+++ components/homePage/FeaturedShoes.tsx
@@ -4,6 +4,7 @@ import path from 'node:path'
 import ShoeCard from '../reusable/ShoeCard'
 import Link from 'next/link'
 import { ArrowUpRight } from 'lucide-react'
+import Header from '../reusable/Header'
 
 async function FeaturedShoes() {
     const filePath = path.join(process.cwd(), "data", "ShoeData.json")
@@ -14,29 +15,8 @@ async function FeaturedShoes() {
     return (
         <section className="px-6 md:px-24 py-18 bg-bgColor">
 
-            {/* Section Header */}
-            <div className="flex flex-row justify-between items-center mb-8 gap-4">
-                <div>
-                    <p className="text-[10px] uppercase tracking-[0.3em] font-semibold mb-1 inline-block px-2 py-0.5 text-primary bg-secondary">
-                        New Arrivals
-                    </p>
-                    <h2 className="text-xl sm:text-3xl md:text-5xl font-black text-white leading-tight">
-                        Featured{" "}
-                        <span className="text-transparent"
-                            style={{ WebkitTextStroke: "1.5px rgba(255,211,60,0.5)" }}>
-                            Collection
-                        </span>
-                    </h2>
-                </div>
+            <Header para='New Arrivals' heading1='Featured' heading2='Collection' button='View All' />
 
-                <Link
-                    href="/shopping"
-                    className="flex items-center gap-1 text-[10px] md:text-xs uppercase tracking-wider font-semibold px-2 py-2 md:px-6 md:py-3 transition-all duration-300 shrink-0 border border-secondary/50 text-secondary hover:bg-secondary hover:text-primary"
-                >
-                    View All
-                    <ArrowUpRight size={12} />
-                </Link>
-            </div>
             {/* Cards Grid */}
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 {featuredShoes.map((shoe: Shoe) => (
