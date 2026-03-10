323754d8b28f18fc9cb4b88d7d550a8153d2df97
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-10 10:59:09 +0500

    Add Categories component to Home page for enhanced shoe category display

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/homePage/HomeHeroSection";
 import FeaturedShoes from "@/components/homePage/FeaturedShoes";
+import Categories from "@/components/homePage/Categories";
 
 export default function Home() {
   return (
     <div className="bg-bgColor">
       <HeroSection/>
       <FeaturedShoes/>
+      <Categories/>
     </div>
   );
 }
