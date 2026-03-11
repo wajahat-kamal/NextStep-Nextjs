536465a59be6eaccc3f9c966ef3eddd63b7ac276
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-11 17:19:51 +0500

    Import FilterdData component into ShoppingPage to integrate filtering functionality directly into the shopping interface.

--- app/shopping/page.tsx
+++ app/shopping/page.tsx
@@ -1,3 +1,4 @@
+import FilterdData from '@/components/shoppingPage/FilterdData';
 import { Metadata } from 'next';
 import React from 'react'
 
@@ -6,14 +7,14 @@ export const metadata: Metadata = {
     description: "",
 };
 
-export default  function ShoppingPage() {
+export default function ShoppingPage() {
     return (
         <div className='min-h-screen'>
             <header className='bg-primary w-full h-75 flex justify-center items-center'>
                 <h1 className='text-white text-7xl'>Shopping</h1>
             </header>
 
-
+            <FilterdData />
         </div>
     )
 }
