c2a27e3d8c541afc44459c8c3f64a7bca3771625
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-11 17:18:51 +0500

    Refactor ShoppingPage to remove shoe data fetching and filter section, moving functionality to FilterdData component for improved organization and separation of concerns.

--- app/shopping/page.tsx
+++ app/shopping/page.tsx
@@ -1,8 +1,4 @@
-import ShoeCard from '@/components/reusable/ShoeCard';
-import { Shoe } from '@/types/Shoe';
 import { Metadata } from 'next';
-import fs from 'node:fs/promises';
-import path from 'node:path';
 import React from 'react'
 
 export const metadata: Metadata = {
@@ -12,9 +8,7 @@ export const metadata: Metadata = {
 
 export default async function ShoppingPage() {
 
-    const filePath = path.join(process.cwd(), "data", "ShoeData.json")
-    const rawData = await fs.readFile(filePath, "utf8")
-    const allShoes: Shoe[] = JSON.parse(rawData)
+    
 
     return (
         <div className='min-h-screen'>
@@ -23,29 +17,7 @@ export default async function ShoppingPage() {
             </header>
 
             {/* filters */}
-            <section className='w-full h-25 px-6 md:px-16 flex md:flex-row flex-col items-center justify-between gap-4'>
-                <input className='placeholder-gray-600 w-[43%] rounded bg-amber-50 py-2 px-4' type="text" placeholder='Search your favrit shoe' />
-                <div className='flex justify-center items-center flex-row gap-2'>
-                    {["Mens", "Womens", "Kids"].map((button, i) => (
-                        <button key={button + i}
-                        className='bg-amber-50 px-4 py-2 rounded text-gray-600 cursor-pointer'
-                        >{button}</button>
-                    ))}
-                </div>
-                <div className='flex justify-center items-center flex-row gap-2'>
-                    {["Formal", "Casual", "Football", "Running"].map((button, i) => (
-                        <button key={button + i}
-                        className='bg-amber-50 px-4 py-2 rounded text-gray-600 cursor-pointer'
-                        >{button}</button>
-                    ))}
-                </div>
-            </section>
-
-            <main className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-6 p-6 md:p-16 md:pt-4'>
-                {allShoes.map((shoe) => (
-                    <ShoeCard key={shoe.id} shoe={shoe} height={true} />
-                ))}
-            </main>
+            
         </div>
     )
 }
