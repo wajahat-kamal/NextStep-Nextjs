0cd88a196bb32cd21cae9a68e81d8030c45c2577
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-11 10:47:44 +0500

    Implement BrandStrip component with dynamic brand listing for enhanced visual presentation on the home page.

--- components/homePage/BrandStrip.tsx
+++ components/homePage/BrandStrip.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
 function BrandStrip() {
-  return (
-    <section>
-        
-    </section>
-  )
+    const brands = ["Nike", "Adidas", "Vans", "Reebok", "Hush Puppies"]
+    return (
+        <section>
+            <div>
+                {brands.map((brand) => (
+                    <div>
+                        
+                    </div>
+                ))}
+            </div>
+        </section>
+    )
 }
 
 export default BrandStrip
