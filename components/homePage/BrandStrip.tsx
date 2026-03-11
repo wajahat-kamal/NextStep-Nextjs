5581e3a1c1b2acda480a0dfb469ee828a51a7554
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-11 10:54:13 +0500

    Refactor BrandStrip component to enhance visual presentation with improved styling and dynamic background colors for brand items, ensuring a more engaging user experience.

--- components/homePage/BrandStrip.tsx
+++ components/homePage/BrandStrip.tsx
@@ -3,11 +3,15 @@ import React from 'react'
 function BrandStrip() {
     const brands = ["Nike", "Adidas", "Vans", "Reebok", "Hush Puppies"]
     return (
-        <section>
-            <div>
-                {brands.map((brand) => (
-                    <div>
-                        
+        <section className='w-full h-40 bg-amber-50 flex justify-center items-center p-16'>
+            <div className='flex justify-center items-center flex-row gap-8'>
+                {brands.map((brand, i) => (
+                    <div key={brand}
+                        className={`p-12 text-3xl ${i % 2 === 0 
+                            ? "bg-primary group-hover:bg-primary/90"
+                            : "bg-secondary group-hover:bg-secondary/90"}`}
+                    >
+                        {brand}
                     </div>
                 ))}
             </div>
