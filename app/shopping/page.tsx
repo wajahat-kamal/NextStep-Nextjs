59d6ba81ab49559db32ebaa55030f92f97d61c73
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-11 17:13:47 +0500

    Refactor filter section in ShoppingPage to adjust input width and button layout, improving spacing and user interaction.

--- app/shopping/page.tsx
+++ app/shopping/page.tsx
@@ -23,8 +23,8 @@ export default async function ShoppingPage() {
             </header>
 
             {/* filters */}
-            <section className='w-full h-25 px-6 md:px-16 flex md:flex-row flex-col items-center gap-4'>
-                <input className='placeholder-gray-600 w-[45%] rounded bg-amber-50 py-2 px-4' type="text" placeholder='Search your favrit shoe' />
+            <section className='w-full h-25 px-6 md:px-16 flex md:flex-row flex-col items-center gap-6'>
+                <input className='placeholder-gray-600 w-[43%] rounded bg-amber-50 py-2 px-4' type="text" placeholder='Search your favrit shoe' />
                 <div className='flex justify-center items-center flex-row gap-2'>
                     {["Mens", "Womens", "Kids"].map((button, i) => (
                         <button key={button + i}
@@ -32,11 +32,12 @@ export default async function ShoppingPage() {
                         >{button}</button>
                     ))}
                 </div>
-                <div>
-                    <button>Formal</button>
-                    <button>Casual</button>
-                    <button>Running</button>
-                    <button>Football</button>
+                <div className='flex justify-center items-center flex-row gap-2'>
+                    {["Formal", "Casual", "Football", "Running"].map((button, i) => (
+                        <button key={button + i}
+                        className='bg-amber-50 px-4 py-2 rounded text-gray-600 cursor-pointer'
+                        >{button}</button>
+                    ))}
                 </div>
             </section>
 
