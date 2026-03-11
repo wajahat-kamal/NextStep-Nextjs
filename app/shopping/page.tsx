82e940fd19f2de4a29c7be60e4427a4ffb19b60e
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-11 17:12:22 +0500

    Refactor filter section in ShoppingPage to improve layout and styling, enhancing user interaction with a search input and category buttons.

--- app/shopping/page.tsx
+++ app/shopping/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export default async function ShoppingPage() {
 
-    const filePath = path.join(process.cwd(), "data", "ShoeData.json") 
+    const filePath = path.join(process.cwd(), "data", "ShoeData.json")
     const rawData = await fs.readFile(filePath, "utf8")
     const allShoes: Shoe[] = JSON.parse(rawData)
 
@@ -23,14 +23,14 @@ export default async function ShoppingPage() {
             </header>
 
             {/* filters */}
-            <section>
-                <div>
-                   <input type="text" /> 
-                </div>
-                <div>
-                    <button>Mens</button>
-                    <button>Womens</button>
-                    <button>Kids</button>
+            <section className='w-full h-25 px-6 md:px-16 flex md:flex-row flex-col items-center gap-4'>
+                <input className='placeholder-gray-600 w-[45%] rounded bg-amber-50 py-2 px-4' type="text" placeholder='Search your favrit shoe' />
+                <div className='flex justify-center items-center flex-row gap-2'>
+                    {["Mens", "Womens", "Kids"].map((button, i) => (
+                        <button key={button + i}
+                        className='bg-amber-50 px-4 py-2 rounded text-gray-600 cursor-pointer'
+                        >{button}</button>
+                    ))}
                 </div>
                 <div>
                     <button>Formal</button>
@@ -40,9 +40,9 @@ export default async function ShoppingPage() {
                 </div>
             </section>
 
-            <main className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-6 p-6 md:p-16'>
+            <main className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-6 p-6 md:p-16 md:pt-4'>
                 {allShoes.map((shoe) => (
-                    <ShoeCard key={shoe.id} shoe={shoe} height={true}/>
+                    <ShoeCard key={shoe.id} shoe={shoe} height={true} />
                 ))}
             </main>
         </div>
