1d64f38e71f1ca9e2f279698c32cbf51a90b6e85
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-11 17:14:37 +0500

    Refactor filter section in ShoppingPage to enhance layout by adjusting spacing and alignment, improving user interaction with search input and category buttons.

--- app/shopping/page.tsx
+++ app/shopping/page.tsx
@@ -23,7 +23,7 @@ export default async function ShoppingPage() {
             </header>
 
             {/* filters */}
-            <section className='w-full h-25 px-6 md:px-16 flex md:flex-row flex-col items-center gap-6'>
+            <section className='w-full h-25 px-6 md:px-16 flex md:flex-row flex-col items-center justify-between gap-4'>
                 <input className='placeholder-gray-600 w-[43%] rounded bg-amber-50 py-2 px-4' type="text" placeholder='Search your favrit shoe' />
                 <div className='flex justify-center items-center flex-row gap-2'>
                     {["Mens", "Womens", "Kids"].map((button, i) => (
