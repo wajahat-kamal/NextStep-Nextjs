82374fcf70c7c325928e3d074aeeaf79b4ad0a90
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-12 14:29:05 +0500

    Implement CardDetailPage to display shoe details based on slug, reading data from ShoeData.json. This change enhances the product detail view by integrating dynamic content retrieval and improving user experience.

--- app/shopping/[slug]/page.tsx
+++ app/shopping/[slug]/page.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Shoe } from '@/types/Shoe';
+import fs from 'node:fs/promises';
+import path from 'node:path';
 
-function CardPage() {
+async function CardDetailPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+
+  const filePath = path.join(process.cwd(), "data", "ShoeData.json")
+  const rawData = await fs.readFile(filePath, "utf8")
+  const allShoes: Shoe[] = JSON.parse(rawData)
+
+  
+
   return (
-    <div>page</div>
+    <div>
+      <h1>Card: { slug }</h1>
+    </div>
   )
 }
 
-export default CardPage
+export default CardDetailPage
