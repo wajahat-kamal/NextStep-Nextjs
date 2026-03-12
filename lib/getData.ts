ac0b0f0d17caea335bbab6183c06733c96085a73
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-12 15:01:43 +0500

    Implement getData function to read and parse ShoeData.json, returning an array of Shoe objects for improved data handling in the application.

--- lib/getData.ts
+++ lib/getData.ts
@@ -1,5 +1,11 @@
-
+import { Shoe } from '@/types/Shoe';
+import fs from 'node:fs/promises';
+import path from 'node:path';
 
 async function getData() {
-    
+    const filePath = path.join(process.cwd(), "data", "ShoeData.json")
+    const raw = await fs.readFile(filePath, "utf8")
+    const allShoes: Shoe[] = JSON.parse(raw)
+    return allShoes;
 }
+export default getData
