8214a6bffbd2aceda31280a64c5e3aad44b4e272
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-13 16:36:11 +0500

    Refactor addToCart logic in cartSlice to improve readability by restructuring conditional statements for item quantity increment and addition.

--- store/cart/cartSlice.ts
+++ store/cart/cartSlice.ts
@@ -34,8 +34,11 @@ const cartSlice = createSlice({
         },
         addToCart: (state, action) => {
             const item = state.cartItems.find((item) => item.id === action.payload.id)
-            if (item) item.quantity += 1;
-            state.cartItems.push({ ...action.payload, quantity: 1 })
+            if (item) {
+                item.quantity += 1;
+            } else {
+                state.cartItems.push({ ...action.payload, quantity: 1 });
+            }
         },
     }
 })
