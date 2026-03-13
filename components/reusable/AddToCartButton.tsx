468ddecda8c6c11b1190fd19a8ab8bde86db5fc7
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-13 16:44:18 +0500

    Implement AddToCartButton component to enable adding shoes to the cart with Redux integration and styled button.

--- components/reusable/AddToCartButton.tsx
+++ components/reusable/AddToCartButton.tsx
@@ -1,9 +1,22 @@
+"use client"
+import { addToCart } from '@/store/cart/cartSlice'
+import { Shoe } from '@/types/Shoe'
+import { ShoppingCart } from 'lucide-react'
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
-function AddToCartButton() {
-  return (
-    <div>AddToCartButton</div>
-  )
+function AddToCartButton({ shoe }: { shoe: Shoe }) {
+    const dispatch = useDispatch()
+    return (
+        <button
+            className="bg-secondary text-primary w-6 h-6 md:w-10 md:h-10 cursor-pointer flex items-center justify-center transition-all duration-200 hover:scale-110 active:scale-95"
+            style={{ backgroundColor: "#FFD33C", color: "#021B41" }}
+            aria-label={`Add ${shoe.name} to cart`}
+            onClick={() => dispatch(addToCart(shoe))}
+        >
+            <ShoppingCart className="md:w-7 md:h-7 h-4 w-4" />
+        </button>
+    )
 }
 
 export default AddToCartButton
