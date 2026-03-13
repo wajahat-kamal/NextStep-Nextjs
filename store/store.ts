7916c43a7352061ed421511cf314535dc4908481
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-13 16:29:47 +0500

    Integrate cartReducer into Redux store configuration to manage cart state effectively.

--- store/store.ts
+++ store/store.ts
@@ -1,15 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import cartReducer from "./cart/cartSlice";
 
 export const store = configureStore({
     reducer: {
-
+        cart: cartReducer
     }
 })
 
 store.subscribe(() => {
     const state = store.getState();
-    localStorage.setItem("cartItems", JSON.parse(state.cart.item))
+    localStorage.setItem("cartItems", JSON.parse(state.cart.cartItems))
 })
 
 export type RootState = ReturnType<typeof store.getState>
