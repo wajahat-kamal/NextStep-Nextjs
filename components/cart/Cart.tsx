b0ab1eeb575213bfc4d47d9efacf16f303c2a680
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-14 13:18:52 +0500

    Refactor Cart component to utilize AnimatePresence for improved animation handling and conditional rendering of the cart overlay, enhancing user experience.

--- components/cart/Cart.tsx
+++ components/cart/Cart.tsx
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import cartImage from "@/assets/cart-image.png"
 import Image from 'next/image';
 import { RootState } from '@/store/store';
-import { motion } from 'motion/react';
+import { motion, AnimatePresence } from 'motion/react';
 import { closeCart } from '@/store/cart/cartSlice';
 import { X } from 'lucide-react';
 
@@ -13,45 +13,48 @@ function Cart() {
   const dispatch = useDispatch()
   const { isOpen } = useSelector((state: RootState) => state.cart)
 
-  if(!isOpen) return null;
-
   return (
-    <div className="fixed inset-0 z-50 bg-black/60 backdrop-blur-sm">
-      {/* Overlay click close */}
-      <div className="absolute inset-0" onClick={() => dispatch(closeCart())} />
+    <AnimatePresence>
+      {isOpen && (
+        <div className="fixed inset-0 z-50 bg-black/60 backdrop-blur-sm">
+          {/* Overlay click close */}
+          <div className="absolute inset-0" onClick={() => dispatch(closeCart())} />
 
-      {/* Drawer */}
-      <motion.div
-        initial={{ opacity: 0, x: 50 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.3, ease: "easeIn" }}
-        className="absolute right-0 top-0 h-full w-80
-          bg-primary border-l border-white/10
-          p-6 shadow-2xl"
-      >
-        {/* Header */}
-        <div className="flex items-center justify-between mb-6">
-          <div className="text-xl md:text-2xl font-semibold text-white flex flex-row gap-1 items-center">
-            <Image
-              src={cartImage}
-              alt="Cart"
-              width={10}
-              height={10}
-              className="h-7 w-7 md:h-10 md:w-10 object-contain opacity-90 pointer-events-none"
-              loading="lazy"
-            />
-            <h3>Your Cart</h3>
-          </div>
-          <button
-            onClick={() => dispatch(closeCart())}
-            className="text-zinc-400 hover:text-white transition"
+          {/* Drawer */}
+          <motion.div
+            initial={{ opacity: 0, x: 300 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 300 }}
+            transition={{ duration: 0.3, ease: "easeIn" }}
+            className="absolute right-0 top-0 h-full w-80
+              bg-primary border-l border-white/10
+              p-6 shadow-2xl"
           >
-            <X />
-          </button>
-        </div>
+            {/* Header */}
+            <div className="flex items-center justify-between mb-6">
+              <div className="text-xl md:text-2xl font-semibold text-white flex flex-row gap-1 items-center">
+                <Image
+                  src={cartImage}
+                  alt="Cart"
+                  width={10}
+                  height={10}
+                  className="h-7 w-7 md:h-10 md:w-10 object-contain opacity-90 pointer-events-none"
+                  loading="lazy"
+                />
+                <h3>Your Cart</h3>
+              </div>
+              <button
+                onClick={() => dispatch(closeCart())}
+                className="text-zinc-400 hover:text-white transition"
+              >
+                <X />
+              </button>
+            </div>
 
-      </motion.div>
-    </div>
+          </motion.div>
+        </div>
+      )}
+    </AnimatePresence>
   )
 }
 
