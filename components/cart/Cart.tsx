7cf15178aadd556825d22e79c7384dd28b8a1400
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-14 13:00:59 +0500

    Add cart header with image and close button to Cart component for enhanced user interface and functionality.

--- components/cart/Cart.tsx
+++ components/cart/Cart.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { useSelector, useDispatch } from "react-redux";
-
+import cartImage from "@/assets/cart-image.png"
 
 function Cart() {
+
   const dispatch = useDispatch()
   const { isOpen } = useSelector((state) => state.cart)
+
   if (!isOpen) return;
+
   return (
     <div className="fixed inset-0 z-50 bg-black/60 backdrop-blur-sm">
       {/* Overlay click close */}
@@ -17,9 +20,28 @@ function Cart() {
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.3, ease: "easeIn" }}
         className="absolute right-0 top-0 h-full w-80
-          bg-(--secondary) border-l border-white/10
+          bg-primary border-l border-white/10
           p-6 shadow-2xl"
       >
+        {/* Header */}
+        <div className="flex items-center justify-between mb-6">
+          <div className="text-xl md:text-2xl font-semibold text-white flex flex-row gap-1 items-center">
+            <Image
+              src={cartImage}
+              alt="Cart"
+              
+              className="h-7 w-7 md:h-10 md:w-10 object-contain opacity-90 pointer-events-none"
+              loading="lazy"
+            />
+            <h3>Your Cart</h3>
+          </div>
+          <button
+            onClick={() => dispatch(isOpen())}
+            className="text-zinc-400 hover:text-white transition"
+          >
+            <X />
+          </button>
+        </div>
 
       </motion.div>
     </div>
