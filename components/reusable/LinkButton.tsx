aa954e99649873d07ee92d4a3f08e32e25c15c00
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-15 11:18:43 +0500

    Update LinkButton component to adjust text size for better responsiveness across different screen sizes.

--- components/reusable/LinkButton.tsx
+++ components/reusable/LinkButton.tsx
@@ -6,7 +6,7 @@ function LinkButton({ text }: { text: string }) {
     return (
         <Link
             href="/shopping"
-            className="flex items-center gap-1 text-[10px] md:text-xs uppercase tracking-wider font-semibold px-2 py-2 md:px-6 md:py-3 transition-all duration-300 shrink-0 border border-secondary/50 text-secondary hover:bg-secondary hover:text-primary"
+            className="flex items-center gap-1 text-[8px] sm:text-[10px] md:text-xs uppercase tracking-wider font-semibold px-2 py-2 md:px-6 md:py-3 transition-all duration-300 shrink-0 border border-secondary/50 text-secondary hover:bg-secondary hover:text-primary"
         >
             {text}
             <ArrowUpRight size={12} />
